731622a690c578ac970ec2333c95dc67f2fdff78
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-12-04 04:53:47 -0300

    Create GH and Now connectors

--- connectors.js
+++ connectors.js
@@ -1,1 +1,31 @@
+const fetch = require('node-fetch');
+
 exports.fs = require('@grial/connector-fs');
+
+exports.gh = async ({ GITHUB_TOKEN }) => {
+  return (url, options = {}) => {
+    const ROOT_URL = 'https://api.github.com';
+
+    return fetch(`${ROOT_URL}${url}`, {
+      ...options,
+      headers: {
+        ...options.headers,
+        Authorization: `token ${GITHUB_TOKEN}`
+      }
+    });
+  };
+};
+
+exports.now = async ({ NOW_TOKEN }) => {
+  return (url, options = {}) => {
+    const ROOT_URL = 'https://api.zeit.co';
+
+    return fetch(`${ROOT_URL}${url}`, {
+      ...options,
+      headers: {
+        ...options.headers,
+        Authorization: `Bearer ${NOW_TOKEN}`
+      }
+    });
+  }
+}
