4dc6d2a14bfeb91f9b7ad933997c7a477d0b7ddb
Sergio Xalambrí <sergiodxa@gmail.com>
2018-03-01 00:41:03 -0500

    Avoid running in dev mode

--- api/connectors/analytics.js
+++ api/connectors/analytics.js
@@ -1,8 +1,12 @@
 const fetch = require("node-fetch");
 const { stringify } = require("querystring");
 
-module.exports = async () => {
+module.exports = async ({ NODE_ENV }) => {
   return ({ action, description, type = "event" }) => {
+    if (NODE_ENV !== "production") {
+      return Promise.resolve();
+    }
+
     const ROOT_URL = "https://analytics.sergiodxa.com";
 
     const query = stringify({ action, description, type });
